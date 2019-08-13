606943c08772da49b08d8b84b9a5d0a603fe297c
Raphael Bost <raphael_bost@alumni.brown.edu>
2019-08-13 20:02:05 +0200

    Add a progress bar for the rust implementation

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -6,13 +6,11 @@ use rand::prelude::*;
 use num_integer::Integer;
 use std::ops::{AddAssign, BitOrAssign, Shr};
 
+use indicatif::{ProgressBar, ProgressStyle};
+
 fn power_of_two<T>(x: T) -> T
 where
-    T: Copy
-        + Integer
-        + AddAssign
-        + Shr<u8>
-        + BitOrAssign<<T as std::ops::Shr<u8>>::Output>,
+    T: Copy + Integer + AddAssign + Shr<u8> + BitOrAssign<<T as std::ops::Shr<u8>>::Output>,
 {
     // super smart and fast way to compute the smallest power of 2 greater than
     // or equal to n.
@@ -33,15 +31,21 @@ where
     return n;
 }
 
-fn alloc(n: usize, m: usize, max_len: usize) -> Vec<usize> {
+fn alloc(n: usize, m: usize, max_len: usize, show_progress: bool) -> Vec<usize> {
     assert!(m > max_len);
     let mut buckets = vec![0; m];
     let mut remaining_elements = n;
 
     let mut rng = thread_rng();
 
+    let pb = ProgressBar::new(n as u64);
+    if show_progress {
+        pb.set_style(ProgressStyle::default_bar()
+        .template("[{elapsed_precise}] {pos}/{len} [{bar:40.cyan/blue}] ({percent}%) | ETA: {eta_precise}")
+        .progress_chars("##-"));
+    }
     while remaining_elements != 0 {
-        let l: usize = rng.gen_range(0, max_len.min(remaining_elements))+1;
+        let l: usize = rng.gen_range(0, max_len.min(remaining_elements)) + 1;
 
         let n_i = power_of_two(l);
         let meta_buckets_counts = m / n_i;
@@ -57,28 +61,40 @@ fn alloc(n: usize, m: usize, max_len: usize) -> Vec<usize> {
 
         let chosen_bucket = if count_b_1 > count_b_2 { b_2 } else { b_1 };
 
-        for j in (n_i * chosen_bucket)..(n_i * (chosen_bucket ) + n_i)  {
+        for j in (n_i * chosen_bucket)..(n_i * (chosen_bucket) + n_i) {
             buckets[j] += 1;
         }
 
         remaining_elements -= l;
+
+        if show_progress {
+            pb.set_position((n - remaining_elements) as u64);
+        }
     }
 
+    if show_progress {
+        pb.finish_with_message("Done!");
+    }
     return buckets;
 }
 
 fn main() {
-    let n = 1<<20;
-    let m = 1<<10;
-    let max_len = 1<<7;
+    let n = 1 << 30;
+    let m = 1 << 20;
+    let max_len = 1 << 17;
 
-    let allocation = alloc(n, m, max_len);
+    let allocation = alloc(n, m, max_len, true);
 
-    let total:usize = allocation.iter().sum();
-    let max_load:usize = allocation.iter().fold(0, |m,x| m.max(*x));
+    let total: usize = allocation.iter().sum();
+    let max_load: usize = allocation.iter().fold(0, |m, x| m.max(*x));
 
-    println!("Total {}", total);
-    println!("Overhead {}", (total as f64)/(n as f64)-1.);
-    println!("Max {}", max_load);
+    println!("Maximum load {}", max_load);
+    println!("Total (with insertion padding) {}", total);
+    println!("Padding overhead {}", (total as f64) / (n as f64) - 1.);
+    println!("Total (with bucket padding) {}", max_load * m);
+    println!(
+        "Complete overhead {}",
+        ((max_load * m) as f64) / (n as f64) - 1.
+    );
     // println!("{:?}", allocation);
 }
