f047730213efd1fc8b1e91982b52ab4248a0fbd8
Raphael Bost <raphael_bost@alumni.brown.edu>
2019-08-13 23:19:01 +0200

    [Rust] Implement iterated experiments. Run multiple allocations to draw some statistics.

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -51,23 +51,91 @@ fn alloc(n: usize, m: usize, max_len: usize, show_progress: bool) -> Vec<usize> 
     buckets
 }
 
+#[derive(Debug, Clone, Copy)]
+struct ExperimentResult {
+    size: usize,
+    max_load: usize,
+}
+
+fn experiment(n: usize, m: usize, max_len: usize, show_progress: bool) -> ExperimentResult {
+    let rand_alloc = alloc(n, m, max_len, show_progress);
+
+    ExperimentResult {
+        size: rand_alloc.iter().sum(),
+        max_load: rand_alloc.iter().fold(0, |m, x| m.max(*x)),
+    }
+}
+
+fn iterated_experiment(
+    iterations: usize,
+    n: usize,
+    m: usize,
+    max_len: usize,
+    show_progress: bool,
+) -> Vec<ExperimentResult> {
+
+    let pb = ProgressBar::new(iterations as u64);
+    if show_progress {
+        pb.set_style(ProgressStyle::default_bar()
+        .template("[{elapsed_precise}] {pos}/{len} [{bar:40.cyan/blue}] ({percent}%) | ETA: {eta_precise}")
+        .progress_chars("##-"));
+    }
+
+    let results: Vec<ExperimentResult> = (0..iterations)
+        .map(|i| {
+            let r = experiment(n, m, max_len, false);
+            if show_progress {
+                pb.set_position((i + 1) as u64);
+            }
+            r
+        })
+        .collect();
+
+    if show_progress {
+        pb.finish_with_message("Done!");
+    }
+
+    results
+}
+
+// fn main() {
+//     let n = 1 << 30;
+//     let m = 1 << 20;
+//     let max_len = 1 << 17;
+
+//     let allocation = alloc(n, m, max_len, true);
+
+//     let total: usize = allocation.iter().sum();
+//     let max_load: usize = allocation.iter().fold(0, |m, x| m.max(*x));
+
+//     println!("Maximum load {}", max_load);
+//     println!("Total (with insertion padding) {}", total);
+//     println!("Padding overhead {}", (total as f64) / (n as f64) - 1.);
+//     println!("Total (with bucket padding) {}", max_load * m);
+//     println!(
+//         "Complete overhead {}",
+//         ((max_load * m) as f64) / (n as f64) - 1.
+//     );
+//     // println!("{:?}", allocation);
+// }
+
 fn main() {
     let n = 1 << 30;
     let m = 1 << 20;
     let max_len = 1 << 17;
+    let iterations = 10;
 
-    let allocation = alloc(n, m, max_len, true);
+    let results = iterated_experiment(iterations, n, m, max_len, true);
 
-    let total: usize = allocation.iter().sum();
-    let max_load: usize = allocation.iter().fold(0, |m, x| m.max(*x));
+    let max_max_load: usize = results.iter().fold(0, |m, x| m.max(x.max_load));
 
-    println!("Maximum load {}", max_load);
-    println!("Total (with insertion padding) {}", total);
-    println!("Padding overhead {}", (total as f64) / (n as f64) - 1.);
-    println!("Total (with bucket padding) {}", max_load * m);
-    println!(
-        "Complete overhead {}",
-        ((max_load * m) as f64) / (n as f64) - 1.
-    );
+    println!("Maximum load {}", max_max_load);
+    // println!("Total (with insertion padding) {}", total);
+    // println!("Padding overhead {}", (total as f64) / (n as f64) - 1.);
+    // println!("Total (with bucket padding) {}", max_load * m);
+    // println!(
+    // "Complete overhead {}",
+    // ((max_load * m) as f64) / (n as f64) - 1.
+    // );
     // println!("{:?}", allocation);
 }
