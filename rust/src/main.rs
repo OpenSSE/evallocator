370f29a879783678f49cd864f55a7678d8fa1508
Raphael Bost <raphael_bost@alumni.brown.edu>
2019-08-13 19:36:22 +0200

    Add a rust version of the allocation simulator

---
+++ rust/src/main.rs
@@ -0,0 +1,84 @@
+extern crate num_integer;
+extern crate rand;
+
+use rand::prelude::*;
+
+use num_integer::Integer;
+use std::ops::{AddAssign, BitOrAssign, Shr};
+
+fn power_of_two<T>(x: T) -> T
+where
+    T: Copy
+        + Integer
+        + AddAssign
+        + Shr<u8>
+        + BitOrAssign<<T as std::ops::Shr<u8>>::Output>,
+{
+    // super smart and fast way to compute the smallest power of 2 greater than
+    // or equal to n.
+    // From
+    // https://www.geeksforgeeks.org/smallest-power-of-2-greater-than-or-equal-to-n/
+    // (method 4)
+    let mut n = x;
+    n += T::one(); // to support n = 2^x
+                   // set to 1 all the bits after the leftmost bit set to 1
+    n |= n >> 1; // set the two leftmost bits to 1
+    n |= n >> 2; // set the 4 leftmost bits to 1
+    n |= n >> 4; // set the 8 leftmost bits to 1
+    n |= n >> 8; // set the 16 leftmost bits to 1
+    n |= n >> 16; // set the 32 leftmost bits to 1
+    n |= n >> 32; // set the 64 leftmost bits to 1
+    n += T::one();
+
+    return n;
+}
+
+fn alloc(n: usize, m: usize, max_len: usize) -> Vec<usize> {
+    assert!(m > max_len);
+    let mut buckets = vec![0; m];
+    let mut remaining_elements = n;
+
+    let mut rng = thread_rng();
+
+    while remaining_elements != 0 {
+        let l: usize = rng.gen_range(0, max_len.min(remaining_elements))+1;
+
+        let n_i = power_of_two(l);
+        let meta_buckets_counts = m / n_i;
+
+        let b_1: usize = rng.gen_range(0, meta_buckets_counts);
+        let b_2: usize = rng.gen_range(0, meta_buckets_counts);
+
+        let slice_1: &[usize] = &buckets[(n_i * b_1)..(n_i * (b_1 + 1))];
+        let count_b_1: usize = slice_1.iter().sum();
+
+        let slice_2: &[usize] = &buckets[(n_i * b_2)..(n_i * (b_2 + 1))];
+        let count_b_2: usize = slice_2.iter().sum();
+
+        let chosen_bucket = if count_b_1 > count_b_2 { b_2 } else { b_1 };
+
+        for j in (n_i * chosen_bucket)..(n_i * (chosen_bucket ) + n_i)  {
+            buckets[j] += 1;
+        }
+
+        remaining_elements -= l;
+    }
+
+    return buckets;
+}
+
+fn main() {
+    let n = 1<<20;
+    let m = 1<<10;
+    let max_len = 1<<7;
+
+    let allocation = alloc(n, m, max_len);
+
+    let total:usize = allocation.iter().sum();
+    let max_load:usize = allocation.iter().fold(0, |m,x| m.max(*x));
+
+    println!("Total {}", total);
+    println!("Overhead {}", (total as f64)/(n as f64)-1.);
+    println!("Max {}", max_load);
+    // println!("{:?}", allocation);
+}
