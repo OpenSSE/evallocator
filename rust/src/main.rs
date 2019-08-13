5ec20bcbf892ef0ac1132f3db5a27e523aae231a
Raphael Bost <raphael_bost@alumni.brown.edu>
2019-08-14 01:58:14 +0200

    [Rust] Implement a better progress bar

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -3,22 +3,20 @@ extern crate rayon;
 
 use rand::prelude::*;
 use rayon::prelude::*;
+use std::sync::atomic::{AtomicUsize,Ordering};
 
-use indicatif::{ProgressBar, ProgressStyle};
+use indicatif::{MultiProgress, ProgressBar, ProgressStyle};
 
-fn alloc(n: usize, m: usize, max_len: usize, show_progress: bool) -> Vec<usize> {
+fn alloc_progress<F>(n: usize, m: usize, max_len: usize, mut progress_callback: F) -> Vec<usize>
+where
+    F: FnMut(usize, usize),
+{
     assert!(m > max_len);
     let mut buckets = vec![0; m];
     let mut remaining_elements = n;
 
     let mut rng = thread_rng();
 
-    let pb = ProgressBar::new(n as u64);
-    if show_progress {
-        pb.set_style(ProgressStyle::default_bar()
-        .template("[{elapsed_precise}] {pos}/{len} [{bar:40.cyan/blue}] ({percent}%) | ETA: {eta_precise}")
-        .progress_chars("##-"));
-    }
     while remaining_elements != 0 {
         let l: usize = rng.gen_range(0, max_len.min(remaining_elements)) + 1;
 
@@ -41,15 +39,32 @@ fn alloc(n: usize, m: usize, max_len: usize, show_progress: bool) -> Vec<usize> 
         }
 
         remaining_elements -= l;
+        progress_callback(n - remaining_elements, l);
+    }
 
+    buckets
+}
+
+fn alloc(n: usize, m: usize, max_len: usize, show_progress: bool) -> Vec<usize> {
+    let pb = ProgressBar::new(n as u64);
+    if show_progress {
+        pb.set_style(ProgressStyle::default_bar()
+        .template("[{elapsed_precise}] {pos}/{len} [{bar:40.cyan/blue}] ({percent}%) | ETA: {eta_precise}")
+        .progress_chars("##-"));
+    }
+
+    let progress_callback = |_, l: usize| {
         if show_progress {
             pb.inc(l as u64);
         }
-    }
+    };
+
+    let buckets = alloc_progress(n, m, max_len, progress_callback);
 
     if show_progress {
         pb.finish_with_message("Done!");
     }
+
     buckets
 }
 
@@ -57,6 +72,23 @@ fn alloc(n: usize, m: usize, max_len: usize, show_progress: bool) -> Vec<usize> 
 struct ExperimentResult {
     size: usize,
     max_load: usize,
+}
+
+fn experiment_progress<F>(
+    n: usize,
+    m: usize,
+    max_len: usize,
+    progress_callback: F,
+) -> ExperimentResult
+where
+    F: FnMut(usize, usize),
+{
+    let rand_alloc = alloc_progress(n, m, max_len, progress_callback);
+
+    ExperimentResult {
+        size: rand_alloc.iter().sum(),
+        max_load: rand_alloc.iter().fold(0, |m, x| m.max(*x)),
+    }
 }
 
 fn experiment(n: usize, m: usize, max_len: usize, show_progress: bool) -> ExperimentResult {
@@ -76,30 +108,41 @@ fn iterated_experiment(
     show_progress: bool,
 ) -> Vec<ExperimentResult> {
 
-    let pb = ProgressBar::new(iterations as u64);
+    let elements_pb = ProgressBar::new((iterations * n) as u64);
     if show_progress {
-        pb.set_style(ProgressStyle::default_bar()
-        .template("[{elapsed_precise}] {pos}/{len} [{bar:40.cyan/blue}] ({percent}%) | ETA: {eta_precise}")
+        elements_pb.set_style(ProgressStyle::default_bar()
+        .template("[{elapsed_precise}] {msg} [{bar:40.cyan/blue}] ({pos}/{len} elts - {percent}%) | ETA: {eta_precise}")
         .progress_chars("##-"));
     }
 
+    let progress_callback = |_, l: usize| {
+        if show_progress {
+            elements_pb.inc(l as u64);
+        }
+    };
+
+    let mut iter_completed = AtomicUsize::new(0);
+
     if show_progress {
-        pb.set_position(0);
+        elements_pb.set_position(0);
+        elements_pb.set_message(&format!("{}/{} iterations",*iter_completed.get_mut(),iterations));
     }
 
-    let results: Vec<ExperimentResult> = (0..iterations).into_par_iter()
+    let results: Vec<ExperimentResult> = (0..iterations)
+        .into_par_iter()
         .map(|_| {
-            let r = experiment(n, m, max_len, false);
+            let r = experiment_progress(n, m, max_len, progress_callback);
 
+            let previous_count = iter_completed.fetch_add(1, Ordering::SeqCst);
             if show_progress {
-                pb.inc(1);
+                elements_pb.set_message(&format!("{}/{} iterations",previous_count+1,iterations));
             }
             r
         })
         .collect();
 
     if show_progress {
-        pb.finish_with_message("Done!");
+        elements_pb.finish_with_message("Done!");
     }
 
     results
@@ -127,22 +170,14 @@ fn iterated_experiment(
 // }
 
 fn main() {
-    let n = 1 << 30;
-    let m = 1 << 20;
-    let max_len = 1 << 17;
-    let iterations = 20;
+    let n = 1 << 20;
+    let m = 1 << 10;
+    let max_len = 1 << 7;
+    let iterations = 200;
 
     let results = iterated_experiment(iterations, n, m, max_len, true);
 
     let max_max_load: usize = results.iter().fold(0, |m, x| m.max(x.max_load));
 
     println!("Maximum load {}", max_max_load);
-    // println!("Total (with insertion padding) {}", total);
-    // println!("Padding overhead {}", (total as f64) / (n as f64) - 1.);
-    // println!("Total (with bucket padding) {}", max_load * m);
-    // println!(
-    // "Complete overhead {}",
-    // ((max_load * m) as f64) / (n as f64) - 1.
-    // );
-    // println!("{:?}", allocation);
 }
