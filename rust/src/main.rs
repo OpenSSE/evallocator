bde06dd226adce46b1de6ea1798a23d242b10371
Raphael Bost <raphael_bost@alumni.brown.edu>
2019-08-13 20:37:29 +0200

    [Rust] Use std::next_power_of_two Replace our custom implementation by std::next_power_of_two

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,35 +1,8 @@
-extern crate num_integer;
 extern crate rand;
 
 use rand::prelude::*;
 
-use num_integer::Integer;
-use std::ops::{AddAssign, BitOrAssign, Shr};
-
 use indicatif::{ProgressBar, ProgressStyle};
-
-fn power_of_two<T>(x: T) -> T
-where
-    T: Copy + Integer + AddAssign + Shr<u8> + BitOrAssign<<T as std::ops::Shr<u8>>::Output>,
-{
-    // super smart and fast way to compute the smallest power of 2 greater than
-    // or equal to n.
-    // From
-    // https://www.geeksforgeeks.org/smallest-power-of-2-greater-than-or-equal-to-n/
-    // (method 4)
-    let mut n = x;
-    n += T::one(); // to support n = 2^x
-                   // set to 1 all the bits after the leftmost bit set to 1
-    n |= n >> 1; // set the two leftmost bits to 1
-    n |= n >> 2; // set the 4 leftmost bits to 1
-    n |= n >> 4; // set the 8 leftmost bits to 1
-    n |= n >> 8; // set the 16 leftmost bits to 1
-    n |= n >> 16; // set the 32 leftmost bits to 1
-    n |= n >> 32; // set the 64 leftmost bits to 1
-    n += T::one();
-
-    return n;
-}
 
 fn alloc(n: usize, m: usize, max_len: usize, show_progress: bool) -> Vec<usize> {
     assert!(m > max_len);
@@ -47,7 +20,7 @@ fn alloc(n: usize, m: usize, max_len: usize, show_progress: bool) -> Vec<usize> 
     while remaining_elements != 0 {
         let l: usize = rng.gen_range(0, max_len.min(remaining_elements)) + 1;
 
-        let n_i = power_of_two(l);
+        let n_i = l.next_power_of_two();
         let meta_buckets_counts = m / n_i;
 
         let b_1: usize = rng.gen_range(0, meta_buckets_counts);
