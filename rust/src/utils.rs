cdda920e00b0d5b83daacc9137a1d8a6b355ae1b
Raphael Bost <raphael_bost@alumni.brown.edu>
2019-08-14 17:53:14 +0200

    Fix the algorithm

--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -1,3 +1,13 @@
+pub fn msb(x: u64) -> u8 {
+    let bits = 64u8;
+    if x != 0 {
+        (bits -1 - (x.leading_zeros() as u8))
+    } else {
+        0u8
+    }
+}
+
+
 #[derive(Debug, Clone, Copy)]
 pub struct Stats {
     pub count: usize,
