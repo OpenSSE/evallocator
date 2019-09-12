dd14d0c89751cf1370b7d5da82306acf62c2194f
Raphael Bost <raphael_bost@alumni.brown.edu>
2019-09-12 19:52:49 +0200

    Added a CLI for the Zipf sampler

--- rust/src/zipf_test.rs
+++ rust/src/zipf_test.rs
@@ -10,6 +10,26 @@ use gnuplot::*;
 extern crate rayon;
 use rayon::prelude::*;
 
+extern crate structopt;
+
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "modular_zipf", about = "Sample a modular Zipf distribution")]
+struct CliArgs {
+    /// The number of elements in the distribution (domain)
+    #[structopt(short = "n")]
+    n: usize,
+    /// The parameter of the Zipf distribution
+    #[structopt(short = "a", long = "alpha")]
+    alpha: f64,
+    /// The modular parameter of the distribution
+    #[structopt(short = "m", long = "mod")]
+    m: usize,
+    /// The number of samples
+    #[structopt(short = "s", long = "samples")]
+    n_samples: usize,
+}
 fn plot_histogram(hist: &[usize]) {
     let mut fg = Figure::new();
     fg.axes2d().boxes(0..=hist.len(), hist, &[]);
@@ -17,29 +37,26 @@ fn plot_histogram(hist: &[usize]) {
 }
 
 fn main() {
-    let alpha = 1.2f64;
-    let n_elts = 1000;
-    let max_count: usize = 100_000;
-    let n_samples: usize = 1_000_000_000;
-    let mut bins = Vec::<std::sync::atomic::AtomicUsize>::with_capacity(n_elts);
+    let args = CliArgs::from_args();
+    println!("{:?}", args);
 
-    for _ in 0..n_elts {
+    let mut bins = Vec::<std::sync::atomic::AtomicUsize>::with_capacity(args.n);
+
+    for _ in 0..args.n {
         bins.push(std::sync::atomic::AtomicUsize::new(0));
     }
 
-    let rng = thread_rng();
-
-    let zipf_distr = ZipfDistribution::new(n_elts, alpha).unwrap();
+    let zipf_distr = ZipfDistribution::new(args.n, args.alpha).unwrap();
     // let zipf_iter = zipf_distr.sample_iter(rng);
 
-    (0..n_samples).into_par_iter().for_each(|_| {
+    (0..args.n_samples).into_par_iter().for_each(|_| {
         let x = zipf_distr.sample(&mut thread_rng());
         bins[x - 1].fetch_add(1, std::sync::atomic::Ordering::SeqCst);
     });
 
     let h: Vec<usize> = bins
         .into_iter()
-        .map(|x| (x.into_inner()) % max_count)
+        .map(|x| (x.into_inner()) % args.m)
         .collect();
     plot_histogram(&h);
 }
